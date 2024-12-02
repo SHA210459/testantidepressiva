66c89abb171e9ce38290527339c9ffb8bb6c6e55
Raiyan <raiyangame1@gmail.com>
2024-12-03 00:13:48 +0100

    cooked

--- routes/chat.py
+++ routes/chat.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 from datetime import datetime
-from extensions import socketio  # Importiere SocketIO
+from extensions import socketio
 from flask_socketio import emit
+import uuid
 
 chat_bp = Blueprint('chat', __name__)
 
@@ -11,7 +12,6 @@ chat_bp = Blueprint('chat', __name__)
 def chat():
     return render_template('chat.html')
 
-# SocketIO-Events
 @socketio.on('send_message')
 def handle_send_message(data):
     username = data.get('username')
@@ -19,12 +19,26 @@ def handle_send_message(data):
     color = data.get('color')
     reply_to = data.get('replyTo')
     timestamp = datetime.now().strftime('%H:%M')
+    message_id = str(uuid.uuid4())
 
-    # Sende Nachricht an alle verbundenen Clients
     emit('receive_message', {
         'msg': message,
         'username': username,
         'time': timestamp,
         'color': color,
-        'replyTo': reply_to
+        'replyTo': reply_to,
+        'message_id': message_id,
+        'reactions': []
+    }, broadcast=True)
+
+@socketio.on('react_message')
+def handle_react_message(data):
+    message_id = data.get('message_id')
+    emoji = data.get('emoji')
+    username = data.get('username')
+
+    emit('react_message', {
+        'message_id': message_id,
+        'emoji': emoji,
+        'username': username
     }, broadcast=True)
